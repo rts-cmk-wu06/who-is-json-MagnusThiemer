8f7c81ecc2b910952537ff01cc2891da16a61d43
MagnusThiemer <thiemer.jensen@gmail.com>
2021-09-13 08:16:03 +0200

    Add json sandox code

--- js/app.js
+++ js/app.js
@@ -1,58 +1,34 @@
-const team = {
-    "teamMembers": [{
-        "fornavn": "Emil",
-        "efternavn": "Kristensen",
-        "alder": 33,
-        "hårfarve": "lys",
-        "skostørrelse": 43
-    }, {
-        "fornavn": "Magnus",
-        "efternavn": "Thiemer",
-        "alder": 31,
-        "hårfarve": "leverpostej",
-        "skostørrelse": 42
-    }, {
-        "fornavn": "Jonas",
-        "efternavn": "Abkjær",
-        "alder": 27,
-        "hårfarve": "kommune",
-        "skostørrelse": 44
-    }, {
-        "fornavn": "Morten",
-        "efternavn": "Sønniksen",
-        "alder": 34,
-        "hårfarve": "rød",
-        "skostørrelse": 44
-    }]
-};
+fetch("./js/team.json")
+    .then(response => response.json())
+    .then(data => {
+        data.teamMembers.forEach(teamMembers => printPersondata(teamMembers));
+    })
 
-/* const team = JSON.parse(team); */
 
 function printPersondata(object){
     const personDataContainer = document.createElement('div');
+    personDataContainer.classList.add("persondata__container")
+    gridContainer.appendChild(personDataContainer);
+
     let newHeading = document.createElement('h2');
-    newHeading.innerHTML = `${object.fornavn} ${object.efternavn}`;
+    newHeading.textContent = `${object.fornavn} ${object.efternavn}`;
+    personDataContainer.appendChild(newHeading);
 
     let newUl = document.createElement('ul');
     newUl.classList.add('persondata__ul')
+    personDataContainer.appendChild(newUl);
     
     let newLiAlder = document.createElement('li')
-    newLiAlder.innerHTML = `Alder: ${object.alder}`;
+    newLiAlder.textContent = `Alder: ${object.alder}`;
+    newUl.appendChild(newLiAlder);
     
     let newLiHårfarve = document.createElement('li')
-    newLiHårfarve.innerHTML = `Hårfarve: ${object.hårfarve}`;
+    newLiHårfarve.textContent = `Hårfarve: ${object.hårfarve}`;
+    newUl.appendChild(newLiHårfarve);
     
     let newLiSkostørrelse = document.createElement('li')
-    newLiSkostørrelse.innerHTML = `Skostørrelse: ${object.skostørrelse}`;
-    
-    personDataContainer.appendChild(newHeading);
-    newUl.appendChild(newLiAlder);
-    newUl.appendChild(newLiHårfarve);
+    newLiSkostørrelse.textContent = `Skostørrelse: ${object.skostørrelse}`;
     newUl.appendChild(newLiSkostørrelse);
-    personDataContainer.appendChild(newUl);
-    personDataContainer.classList.add("persondata__container")
-    
-    gridContainer.appendChild(personDataContainer);
 };
 
 function createGridContainer(){
@@ -65,6 +41,6 @@ createGridContainer();
 
 let gridContainer = document.querySelector('.grid');
 
-team.teamMembers.forEach(teamMembers => console.log(teamMembers.fornavn));
+/* team.teamMembers.forEach(teamMembers => console.log(teamMembers.fornavn));
 team.teamMembers.forEach(teamMembers => console.log(`${teamMembers.fornavn} ${teamMembers.efternavn}`));
-team.teamMembers.forEach(teamMembers => printPersondata(teamMembers));
+team.teamMembers.forEach(teamMembers => printPersondata(teamMembers)); */
